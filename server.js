498bf362d2d0e0cd29d6d0e196076a7bb6681672
VDevSecOps212023 <sanda_vinay@persistent.com>
2026-02-05 18:31:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World from Azure App Service!");
+}).listen(port);
+
+console.log("Server running on port " + port);
